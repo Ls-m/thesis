2c46b1bc62525a1467b84069842428428ef54ed3
ls-m <elham.fr80@gmail.com>
2025-07-23 21:29:26 +0330

    execute commands

--- src/execute.sh
+++ src/execute.sh
@@ -23,4 +23,9 @@ python train_subject_wise.py --config ../configs/improved_config.yaml --override
 
 
 
-python train_subject_wise.py --config ../configs/improved_config.yaml --override model.name=ImprovedTransformer --override training.max_epochs=150 --override data.csv_folder=capno --override  data.sampling_rate=300 --override data.segment_length=2400 --override preprocessing.downsample.target_rate=30 --override model.input_size=240 --fold 0009_8min --override logging.experiment_name=ImprovedTransformer_b64_e150_capno_experiment --override model.dropout=0.2
+python train_enhanced.py --config ../configs/improved_config.yaml --override model.name=AttentionCNN_LSTM --override training.max_epochs=50 --override data.csv_folder=capno --override  data.sampling_rate=300 --override data.segment_length=2400 --override preprocessing.downsample.target_rate=30 --override model.input_size=240 --override logging.experiment_name=AttentionCNN_LSTM_debugged_b64_d4_e50_capno_5f_experiment --override model.dropout=0.4 --cv-method k_fold --n-folds 5
+
+
+python train_enhanced.py --config ../configs/improved_config.yaml --override model.name=ImprovedTransformer --override training.max_epochs=50 --override data.csv_folder=capno --override  data.sampling_rate=300 --override data.segment_length=4800 --override preprocessing.downsample.target_rate=30 --override model.input_size=480 --override logging.experiment_name=ImprovedTransformer_debugged_b64_d45_capno_5f_experiment --override model.dropout=0.45 --override preprocessing.bandpass_filter.high_freq=1.0 --cv-method k_fold --n-folds 5
+
+
